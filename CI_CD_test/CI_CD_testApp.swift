9d508810f666be50e4f99be2d15942ea0ead45b5
bob <bob@>
2023-08-26 08:59:51 +0300

    init on develop

---
+++ CI_CD_test/CI_CD_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  CI_CD_testApp.swift
+//  CI_CD_test
+//
+//  Created by A One Way To Allah on 26/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct CI_CD_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CI_CD_testTests/CI_CD_testTests.swift
@@ -0,0 +1,20 @@
+//
+//  CI_CD_testTests.swift
+//  CI_CD_testTests
+//
+//  Created by A One Way To Allah on 26/08/2023.
+//
+
+import XCTest
+@testable import CI_CD_test
+
+final class CI_CD_testTests: XCTestCase {
+
+   
+    func testExample() throws {
+        XCTAssertEqual(1+1,2)
+    }
+
+    
+
+}
